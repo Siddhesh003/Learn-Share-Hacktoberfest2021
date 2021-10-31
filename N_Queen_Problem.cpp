4bd43d6630c9f074bf9c5892babb43a52d4bf6a5
Siddhesh Ambre <64468240+Siddhesh003@users.noreply.github.com>
2021-10-31 23:09:21 +0530

    N_Queen_Problem.cpp
    Competitive programming

---
+++ N_Queen_Problem.cpp
@@ -0,0 +1,59 @@
+//This problem is to find an arrangement of N queens on a chess board, such that no queen can attack any other queens on the board.
+
+//The chess queens can attack in any direction as horizontal, vertical, horizontal and diagonal way.
+
+//A binary matrix is used to display the positions of N Queens, where no queens can attack other queens. Here, we solve 8 queens problem.
+
+
+
+
+#include<iostream>
+using namespace std;
+#define N 4
+void printBoard(int board[N][N]) {
+   for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++)
+         cout << board[i][j] << " ";
+         cout << endl;
+   }
+}
+bool isValid(int board[N][N], int row, int col) {
+   for (int i = 0; i < col; i++) //check whether there is queen in the left or not
+      if (board[row][i])
+         return false;
+   for (int i=row, j=col; i>=0 && j>=0; i--, j--)
+      if (board[i][j]) //check whether there is queen in the left upper diagonal or not
+         return false;
+   for (int i=row, j=col; j>=0 && i<N; i++, j--)
+      if (board[i][j]) //check whether there is queen in the left lower diagonal or not
+         return false;
+   return true;
+}
+bool solveNQueen(int board[N][N], int col) {
+   if (col >= N) //when N queens are placed successfully
+      return true;
+   for (int i = 0; i < N; i++) { //for each row, check placing of queen is possible or not
+      if (isValid(board, i, col) ) {
+         board[i][col] = 1; //if validate, place the queen at place (i, col)
+         if ( solveNQueen(board, col + 1)) //Go for the other columns recursively
+            return true;
+         board[i][col] = 0; //When no place is vacant remove that queen
+      }
+   }
+   return false; //when no possible order is found
+}
+bool checkSolution() {
+   int board[N][N];
+   for(int i = 0; i<N; i++)
+   for(int j = 0; j<N; j++)
+   board[i][j] = 0; //set all elements to 0
+   if ( solveNQueen(board, 0) == false ) { //starting from 0th column
+      cout << "Solution does not exist";
+      return false;
+   }
+   printBoard(board);
+   return true;
+}
+int main() {
+   checkSolution();
+}
